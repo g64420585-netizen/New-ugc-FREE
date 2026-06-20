b5e8677b1e71ba5fd4c397479c1284377e9cc659
g64420585-netizen <g64420585@gmail.com>
2026-06-20 20:51:01 -0300

    feat: add src/vite-env.d.ts

---
+++ src/vite-env.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="vite/client" />
+declare module "*.png" { const s: string; export default s; }
+declare module "*.jpg" { const s: string; export default s; }
+declare module "*.svg" { const s: string; export default s; }
