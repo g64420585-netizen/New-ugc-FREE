fee7a32e3a1c42289c9d46ad2b5c2b5a8b837750
g64420585-netizen <g64420585@gmail.com>
2026-06-20 15:14:04 -0300

    sync: App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,33 @@
-import { Switch, Route, Router as WouterRouter } from "wouter";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import NotFound from "@/pages/not-found";
-import Home from "@/pages/Home";
+import { Switch, Route } from "wouter";
+  import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+  import { Toaster } from "@/components/ui/toaster";
+  import { TooltipProvider } from "@/components/ui/tooltip";
+  import NotFound from "@/pages/not-found";
+  import Home from "@/pages/Home";
 
-const queryClient = new QueryClient();
+  const queryClient = new QueryClient();
 
-function Router() {
-  return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
+  function Router() {
+    return (
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route component={NotFound} />
+      </Switch>
+    );
+  }
 
-function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <WouterRouter base={import.meta.env.BASE_URL.replace(/\/$/, "")}>
+  function App() {
+    return (
+      <QueryClientProvider client={queryClient}>
+        <TooltipProvider>
           <div className="dark">
             <Router />
           </div>
-        </WouterRouter>
-        <Toaster />
-      </TooltipProvider>
-    </QueryClientProvider>
-  );
-}
+          <Toaster />
+        </TooltipProvider>
+      </QueryClientProvider>
+    );
+  }
 
-export default App;
+  export default App;
+  
